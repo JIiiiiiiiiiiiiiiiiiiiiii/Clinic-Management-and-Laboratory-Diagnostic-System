6133d6da38ab242d667d9a6fe0b040dfdb6fa26d
Ronnel <ronnel@clinic.com>
2025-10-15 11:28:40 +0800

    Add simple test component to isolate React import issues

---
+++ resources/js/components/dashboard/TestComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function TestComponent() {
+    return (
+        <div className="bg-yellow-100 p-4 rounded-lg">
+            <h3 className="text-lg font-semibold text-yellow-800">Test Component</h3>
+            <p className="text-yellow-700">This is a simple test component</p>
+        </div>
+    );
+}
